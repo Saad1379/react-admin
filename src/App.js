8fc4d77df234145c4cfef70e49290c13998336d3
Saad1379 <sheikh.saad.riaz@gmail.com>
2022-05-28 16:14:52 +0500

    react admin app4

--- src/App.js
+++ src/App.js
@@ -13,8 +13,31 @@ function App() {
   const { darkMode } = useContext(DarkModeContext);
 
   return (
-    <div>
-gfee
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
